8eaed3da46c502c064fde226cb1e758ebdd355fd
Kareem-Mohamed0 <KAREEM.mohamed3321@el-eng.menofia.edu.eg>
2024-10-22 21:42:14 +0300

    implement MUL & Sub

---
+++ Coding Tracker/ICalcFnuctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Tracker
+{
+    internal interface ICalcFnuctions
+    {
+        public int Mul (int a, int b);
+        public int Sub(int a, int b);
+    }
+}
